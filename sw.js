fda1e2838a0d3e25dcb06646cc40033ac67ae6ad
Mahmoud I.Bhar <mahmoudbhar0@gmail.com>
2026-09-10 14:23:12 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,27 @@
 const CACHE_NAME = 'islamic-stories-v1';
 const assetsToCache = [
-  './index.html',
-  './style.css',
-  './app.js',
-  './stories.js',
-  './manifest.json',
-  './mah.png'
+    './index.html',
+    './style.css',
+    './app.js',
+    './stories.js',
+    './manifest.json',
+    './mah.png'
 ];
 
-// تثبيت الخدمة وحفظ الملفات في الذاكرة المؤقتة
+// تثبيت التطبيق وتخزين الملفات
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assetsToCache);
-    })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(assetsToCache);
+        })
+    );
 });
 
-// جلب الملفات من التخزين المؤقت عند انقطاع الإنترنت
+// تفعيل الخدمة واستجابة الطلبات بدون إنترنت
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
 });
